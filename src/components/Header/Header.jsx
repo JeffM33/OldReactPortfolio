909fafdfae93121b05aa11e1c8b012985d43dbd0
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-10-21 11:05:43 -0500

    made edits to scss installed icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,16 @@ export default function Header() {
     return (
         <div className='header'>
             <div className='wrapper'>
-                
+                <div className="left">
+                    <a href='#intro' className='logo'> genius.</a>
+                    <div className="itemContainer">
+                        
+                    </div>
+                </div>
+
+                <div className="right">
+                    
+                </div>
             </div>
         </div>
     )
